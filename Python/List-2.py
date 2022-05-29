b6332fdb65f652bef2dd693f01ec4e91279edf37
Nesta Fletcher <89424122+nrfletcher@users.noreply.github.com>
2022-05-29 19:56:23 -0400

    Update List-2.py

--- Python/List-2.py
+++ Python/List-2.py
@@ -8,3 +8,14 @@ def count_evens(nums):
     if nums[i] % 2 == 0:
       count += 1
   return count
+
+def big_diff(nums):
+  biggest = -100
+  smallest = 100
+  for i in nums:
+    if i > biggest:
+      biggest = i
+    if i < smallest:
+      smallest = i
+
+  return biggest - smallest
