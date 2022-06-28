34735a4f898777f846cc3e7a9d0647de1684779b
N R Fletcher <89424122+nrfletcher@users.noreply.github.com>
2022-06-28 18:43:26 -0400

    Update String-2.py

--- Python/String-2.py
+++ Python/String-2.py
@@ -42,3 +42,10 @@ def count_code(str):
       cCount += 1
     
   return cCount
+
+def xyz_there(str):
+  count = 0
+  for i in range(len(str)):
+    if str[i:i+3] == 'xyz' and str[i-1] != '.':
+      return True
+  return False
