ed3b5bc2faee1d73d64b5ef90e03f6ec5c688a82
Nesta Fletcher <89424122+nrfletcher@users.noreply.github.com>
2022-07-08 22:53:17 -0400

    Update functional-1.java

--- Java/functional-1.java
+++ Java/functional-1.java
@@ -12,3 +12,33 @@ public List<String> copies3(List<String> strings) {
   strings.replaceAll(str -> str + str + str);
   return strings;
 }
+
+public List<Integer> rightDigit(List<Integer> nums) {
+  nums.replaceAll(n -> n % 10);
+  return nums;
+}
+
+public List<Integer> square(List<Integer> nums) {
+  nums.replaceAll(n -> n*n);
+  return nums;
+}
+
+public List<String> moreY(List<String> strings) {
+  strings.replaceAll(n -> "y" + n + "y");
+  return strings;
+}
+
+public List<Integer> math1(List<Integer> nums) {
+  nums.replaceAll(n -> (n+1)*10);
+  return nums;
+}
+
+public List<String> noX(List<String> strings) {
+  strings.replaceAll(n -> n.replace("x", ""));
+  return strings;
+}
+
+public List<String> lower(List<String> strings) {
+  strings.replaceAll(n -> n.toLowerCase());
+  return strings;
+}
