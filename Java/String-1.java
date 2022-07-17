f4f6b2595a7c2cc303864674ae9a75fadf79cb69
Nesta Riley Fletcher <89424122+nrfletcher@users.noreply.github.com>
2022-07-17 19:30:01 -0400

    Update String-1.java

--- Java/String-1.java
+++ Java/String-1.java
@@ -1,3 +1,12 @@
 public String middleThree(String str) {
   return str.substring((str.length()/2)-1, str.length()/2+2);
 }
+
+public String startWord(String str, String word) {
+  if(word.length() > str.length()) return "";
+  if(str.substring(1, word.length()).equals(word.substring(1, word.length()))) {
+    return str.substring(0, word.length());
+  } else {
+    return "";
+  }
+}
