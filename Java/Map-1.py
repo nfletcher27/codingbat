701d96addc8d6c62705df40320a7253a4f5cb823
Nesta Riley Fletcher <89424122+nrfletcher@users.noreply.github.com>
2022-07-18 15:10:11 -0400

    Update Map-1.py

--- Java/Map-1.py
+++ Java/Map-1.py
@@ -3,3 +3,26 @@ public Map<String, String> mapShare(Map<String, String> map) {
   if(map.containsKey("a")) map.put("b", map.get("a"));
   return map;
 }
+
+public Map<String, String> mapAB(Map<String, String> map) {
+  if(map.containsKey("a") && map.containsKey("b")) {
+    map.put("ab", map.get("a") + map.get("b"));
+  }
+  return map;
+}
+
+public Map<String, String> mapBully(Map<String, String> map) {
+  if(map.containsKey("a")) {
+    map.put("b", map.get("a"));
+    map.put("a", "");
+  }
+  return map;
+}
+
+public Map<String, String> topping1(Map<String, String> map) {
+  if(map.containsKey("ice cream")) {
+    map.put("ice cream", "cherry");
+  }
+  map.put("bread", "butter");
+  return map;
+}
