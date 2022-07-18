2c75af8512443844b5eea9679070e3767f490c22
Nesta Riley Fletcher <89424122+nrfletcher@users.noreply.github.com>
2022-07-18 15:31:55 -0400

    Update Logic-2.py

--- Python/Logic-2.py
+++ Python/Logic-2.py
@@ -64,3 +64,11 @@ def round10(num):
       return 10
     else:
       return 0
+    
+def close_far(a, b, c):
+  if(abs(a - b) >= 2 and abs(b - c) >= 2 and abs(a - c) <= 1):
+    return True
+  elif(abs(a - c) >= 2 and abs(b - c) >= 2 and abs(a - b) <= 1):
+    return True
+  else:
+    return False
