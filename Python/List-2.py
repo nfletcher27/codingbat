4a28248b14af1e0978cb2ca12370e72569631921
Nesta Riley Fletcher <89424122+nrfletcher@users.noreply.github.com>
2022-07-26 18:41:22 -0400

    Update List-2.py

--- Python/List-2.py
+++ Python/List-2.py
@@ -19,3 +19,10 @@ def big_diff(nums):
       smallest = i
 
   return biggest - smallest
+
+def has22(nums):
+  for i in range(len(nums)-1):
+    if nums[i] == 2 and nums[i+1] == 2:
+      return True
+      
+  return False
