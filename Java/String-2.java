1c01b4f43dd0759f5efafd09ee079c0ebe00bc80
Nesta Riley Fletcher <89424122+nrfletcher@users.noreply.github.com>
2022-08-10 21:55:36 -0400

    Update String-2.java

--- Java/String-2.java
+++ Java/String-2.java
@@ -9,3 +9,19 @@ public String mixString(String a, String b) {
                                      b.substring(len, b.length());
   return fin;
 }
+
+public String zipZap(String str) {
+  String str2 = "";
+  if(str.length() < 3) return str;
+  
+  for(int i = 0; i < str.length(); i++) {
+    if(str.charAt(i) == 'z' && str.charAt(i+2) == 'p') {
+      str2 += "zp";
+      i += 2;
+    } else {
+      str2 += str.charAt(i);
+    }
+  }
+  
+  return str2;
+}
